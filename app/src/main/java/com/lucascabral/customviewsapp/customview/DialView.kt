d433e32c63d7a01898e0b8c2f096a0986cc9aed9
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-06-09 11:16:40 -0300

    Acidionado custom attributes para a view.

--- app/src/main/java/com/lucascabral/customviewsapp/customview/DialView.kt
+++ app/src/main/java/com/lucascabral/customviewsapp/customview/DialView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
 import com.lucascabral.customviewsapp.R
 import kotlin.math.cos
 import kotlin.math.min
@@ -37,6 +38,10 @@ class DialView @JvmOverloads constructor(
 
     private val pointPosition: PointF = PointF(0.0f, 0.0f)
 
+    private var fanSpeedLowColor = 0
+    private var fanSpeedMediumColor = 0
+    private var fanSpeedMaxColor = 0
+
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         textAlign = Paint.Align.CENTER
@@ -46,6 +51,12 @@ class DialView @JvmOverloads constructor(
 
     init {
         isClickable = true
+
+        context.withStyledAttributes(attrs, R.styleable.DialView) {
+            fanSpeedLowColor = getColor(R.styleable.DialView_fanColor1, 0)
+            fanSpeedMediumColor = getColor(R.styleable.DialView_fanColor2, 0)
+            fanSpeedMaxColor = getColor(R.styleable.DialView_fanColor3, 0)
+        }
     }
 
     override fun performClick(): Boolean {
@@ -72,7 +83,12 @@ class DialView @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        paint.color = if (fanSpeed == FanSpeed.OFF) Color.GRAY else Color.GREEN
+        paint.color = when (fanSpeed) {
+            FanSpeed.OFF -> Color.GRAY
+            FanSpeed.LOW -> fanSpeedLowColor
+            FanSpeed.MEDIUM -> fanSpeedMediumColor
+            FanSpeed.HIGH -> fanSpeedMaxColor
+        }
         canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, paint)
         // Draw the indicator circle.
         val markerRadius = radius + RADIUS_OFFSET_INDICATOR
